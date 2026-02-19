50b9bf172fe66a344b7233b9a9218be35fc4e03c
Oleksandr Khokhulia <ok797738@southeast.edu>
2026-02-18 19:04:26 -0600

    Add question to the questions pool form created

--- web/scripts/script.js
+++ web/scripts/script.js
@@ -11,6 +11,9 @@ const drillCreateCloseBtn = document.querySelector('#drill_create_close_btn');
 const cancelBtn = document.querySelector('#login-btn-cancel');
 const addQuestionBtn = document.querySelector('#add_question');
 const createAnswerBtn = document.querySelector('#create_answer');
+const addAnswerBtn = document.querySelector('#add_answer');
+
+const answersPool = [];
 
 //separated button logic so that it would allow a cancel to execute, with the original logic " if(signInBtn || getStarted) {} " the condition was always true, leading to logic issues with the cancel button
 signInBtn?.addEventListener('click', (e) => signInForm(e));
@@ -19,7 +22,37 @@ cancelBtn?.addEventListener('click', (e) => cancelSignInForm(e));
 drillCreateBtn?.addEventListener('click', () => document.querySelector('.drill_create_container').style.display = 'flex');
 drillCreateCloseBtn?.addEventListener('click', () => document.querySelector('.drill_create_container').style.display = 'none');
 addQuestionBtn?.addEventListener('click', () => document.querySelector('.add_qestion_form').style.display = 'flex');
-createAnswerBtn?.addEventListener('click', () => document.querySelector('.create_answer_container').style.display = 'flex');
+createAnswerBtn?.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('.create_answer_container').style.display = 'flex';
+});
+
+
+addAnswerBtn?.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(answersPool);
+    const answerText = document.querySelector('#answer').value;
+    const isCorrect = document.querySelector('#isCorrect').checked;
+    const answer = {
+        'answer': answerText,
+        'isCorrect': isCorrect
+    };
+    answersPool.push(answer);
+    document.querySelector('#answer_list').innerHTML = '';
+
+    answersPool.forEach(answer => {
+        const li = document.createElement('li');
+        li.textContent = answer['answer'];
+        if (answer['isCorrect']) li.classList.add('correct_answer');
+
+        document.querySelector('#answer_list').append(li);        
+    });
+    
+    document.querySelector('.create_answer_container').style.display = 'none';   
+    document.querySelector('#answers_input').value = answersPool;
+});
+
+
 
 function signInForm(e) {
     e.preventDefault();
